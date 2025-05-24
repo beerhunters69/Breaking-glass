be60a15c48ed26e2d63ffeb74baf824af05cc524
BlobAWV <martin.gorelov@yandex.ru>
2025-05-25 01:56:48 +0300

    Removed some recipes, added duraliminium gear

--- kubejs/server_scripts/recipes/removed_recipes.js
+++ kubejs/server_scripts/recipes/removed_recipes.js
@@ -1,10 +1,17 @@
 priority: 11
 onEvent('recipes', event => {
 
-	//убрать молот из эмендатус энигматики
 	event.remove({id: 'emendatusenigmatica:enigmatic_hammer'})
     event.remove({id: 'create:crafting/kinetics/deployer'})
 	event.remove({id: 'create:crafting/kinetics/mechanical_press'})
 	event.remove({id: 'twilightforest:equipment/knightmetal_pickaxe'})
-
+	event.remove({id: 'immersiveengineering:crafting/constantan_mix'})
+	event.remove({id: 'emendatusenigmatica:alloy_dust/constantan'})
+	event.remove({id: 'immersiveengineering:alloysmelter/constantan'})
+	event.remove({id: 'tconstruct:smeltery/alloys/molten_constantan'})
+	event.remove({id: 'immersiveengineering:crafting/ingot_constantan_from_dust'})
+	event.remove({id: 'immersiveengineering:crafting/ingot_constantan_from_dust_from_blasting'})
+	event.remove({id: 'emendatusenigmatica:ingot_from_dust/smelting/constantan'})
+	event.remove({id: 'emendatusenigmatica:ingot_from_dust/blasting/constantan'})
+	event.remove({id: ''})
 })
