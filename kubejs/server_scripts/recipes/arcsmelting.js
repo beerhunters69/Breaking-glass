b8ff0e333604bbf90105eab5cd09a09579fcb36d
Magma0Block <ya.andrew112@gmail.com>
2025-05-25 02:58:58 +0300

    arc smelting fixed

--- kubejs/server_scripts/recipes/arcsmelting.js
+++ kubejs/server_scripts/recipes/arcsmelting.js
@@ -1,3 +1,4 @@
 onEvent('recipes', event => {
-    event.recipes.immersiveengineering.arc_furnace("kubejs:duraluminium_ingot", "kubejs:duraluminium_dust", "minecraft:air", slag)
+    event.recipes.immersiveengineering.arc_furnace("kubejs:duraluminium_ingot", "kubejs:duraluminium_dust")
+    event.recipes.immersiveengineering.arc_furnace('immersiveengineering:ingot_steel', 'minecraft:iron_ingot',Item.of("minecraft:coal",2),'immersiveengineering:slag')
 })
