0aeef3f7027c7fdd1138b6cf168e6f9264780a74
Magma0Block <ya.andrew112@gmail.com>
2025-05-25 03:53:23 +0300

    sync

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -1,7 +1,5 @@
 // priority: 0
 
-console.info("Хабиба Мир запустился")
-
 onEvent("item.registry", event => {
 	// Register new items here
 	// event.create("example_item").displayName("Example Item")
