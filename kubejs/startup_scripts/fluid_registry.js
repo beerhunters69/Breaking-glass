9c2511cf07fb44fbcebe42701adb3db75518468f
BlobAWV <martin.gorelov@yandex.ru>
2025-06-01 01:30:23 +0300

    Fixed molten angelic alloy

--- kubejs/startup_scripts/fluid_registry.js
+++ kubejs/startup_scripts/fluid_registry.js
@@ -1,6 +1,6 @@
 //0x630991
 
 onEvent('fluid.registry', event => {
-  event.create('molten_angelic_alloy').displayName('Molten Angelic Alloy').stillTexture('kubejs:block/molten_angelic_alloy_still').flowingTexture('kubejs:block/molten_angelic_alloy_flow')
+  event.create('molten_angelic_alloy').displayName('Molten Angelic Alloy').textureStill('kubejs:block/molten_angelic_alloy_still').textureFlowing('kubejs:block/molten_angelic_alloy_flow').bucketColor(0x630991)
 
 })
