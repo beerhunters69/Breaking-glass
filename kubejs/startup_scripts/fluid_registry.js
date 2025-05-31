27fc21182acb90daad2a9d3f37ef69019cc10d34
Magma0Block <ya.andrew112@gmail.com>
2025-05-31 23:09:27 +0300

    molten angelic alloy

---
+++ kubejs/startup_scripts/fluid_registry.js
@@ -0,0 +1,14 @@
+onEvent('fluid.registry', event => {
+      
+    
+    
+    event.create('molten_angelic_alloy')
+    .thickTexture(0x311d3b)
+    .bucketColor(0x311d3b)
+    .displayName('Molten Angelic Alloy')
+    .rarity(epic)
+    .luminosity(1)
+
+    
+//#311d3b
+})
