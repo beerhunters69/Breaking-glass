991e4f18f7930727fa5d0a3e5a41ab197ded72f4
BlobAWV <martin.gorelov@yandex.ru>
2025-06-06 22:17:00 +0300

    Added textures for molten wgnc and cwgnc

--- kubejs/startup_scripts/fluid_registry/crude_molten_weapon_grade_netherite_compound.js
+++ kubejs/startup_scripts/fluid_registry/crude_molten_weapon_grade_netherite_compound.js
@@ -1,4 +1,4 @@
 onEvent('fluid.registry', event => {
-  event.create('molten_cwgnc').displayName('Molten Crude Weapon Grade Netherite Compund')
+  event.create('molten_cwgnc').displayName('Molten Crude Weapon Grade Netherite Compound').textureStill('kubejs:block/molten_cwgnc_still').textureFlowing('kubejs:block/molten_cwgnc_flow').bucketColor(0x4C494B)
 
 })
